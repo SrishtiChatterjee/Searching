dc79dfcd4d2b1eaade7935fdc9673d2ebe8e84c6
SrishtiChatterjee <102905446+SrishtiChatterjee@users.noreply.github.com>
2023-03-15 15:42:02 +0530

    Update binarysearch.c

--- binarysearch.c
+++ binarysearch.c
@@ -2,12 +2,12 @@
 int main()
 {
 int i, l, h, mid, n, key, array[100];
-printf("Enter number of elementsn");
+printf("Enter number of elements: ");
 scanf("%d",&n);
-printf("Enter %d integersn", n);
+printf("Enter %d integers: \n", n);
 for(i = 0; i < n; i++)
 scanf("%d",&array[i]);
-printf("Enter value to findn");
+printf("Enter value to find: ");
 scanf("%d", &key);
 l = 0;
 h = n - 1;
@@ -16,7 +16,7 @@ while (l <= h) {
 if(array[m] < key)
 l = m + 1;
 else if (array[m] == key) {
-printf("%d found at location %d.n", key, m+1);
+printf("%d found at location %d", key, m+1);
 break;
 }
 else
@@ -24,6 +24,6 @@ h = m - 1;
 m = (l + h)/2;
 }
 if(l > h)
-printf("Not found! %d isn't present in the list.n", key);
+printf("Not found! %d isn't present in the list", key);
 return 0;
 }
